87554c92513608ed2f6afda92f63cbad2f45d563
peterzieba <pzieba@wickerparkdata.com>
2023-10-19 16:26:41 -0500

    Adding a simple script to convert an EDIF .edn file to Verilog using spydrnet.

---
+++ edif-parse/edif2v.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import spydrnet as sdn
+from spydrnet.util.selection import Selection
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert edif netlist into verilog')
+    parser.add_argument('netlist', help='input netlist', type=str)
+    args = parser.parse_args()
+
+    netlist = sdn.parse(args.netlist)
+
+    verilog_out_filename=os.path.splitext(args.netlist)[0] + ".v"
+    sdn.compose(netlist, verilog_out_filename)
+
+if __name__ == "__main__":
+    main()
